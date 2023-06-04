50f0fd0b97a71d59340ef644600dce1e661dd914
MOHDSHEES <mohd.shees100@gmail.com>
2023-06-04 20:11:43 +0530

    no cache trying 2

--- backend/server.js
+++ backend/server.js
@@ -38,9 +38,6 @@ app.use("/api", blogRoute);
 // app.use("/api", emailRoutes);
 // app.use("/api/payment", paymentRoutes);
 app.get("*", (req, res) => {
-  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-  res.setHeader("Pragma", "no-cache");
-  res.setHeader("Expires", "0");
   res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
 });
 
